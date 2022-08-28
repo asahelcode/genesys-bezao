91a7b0237e8a491a284ed3a1d9038b29c3bb7239
Henry <97439022+asahelcode@users.noreply.github.com>
2022-08-28 19:18:00 +0000

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -20,21 +20,21 @@ const Blog = () => {
                     <h2> <span style={{ color: "#da3743" }}>Design</span> <span style={{ color: "rgba(85, 100, 128, 1)" }}>01 October 2019</span></h2>
                     <h1>Better Than Takeout Kung Pao Chicken</h1>
                     <p>This spicy, tangy, and sweet better than takeout no peanut kung pao chicken stir fry recipe </p>
-                    <p style={{ color: "#da3743", fontWeight: "700", textTransform: "uppercase" }}>Read More</p>
+                    <p style="readmore__button">Read More</p>
                 </div>
                 <div className="blog__card">
                     <img src={Fries} alt="" />
                     <h2> <span style={{ color: "#da3743" }}>Design</span> <span style={{ color: "rgba(85, 100, 128, 1)" }}>01 October 2019</span></h2>
                     <h1>The Best Sesame Soy Broccoli Salad</h1>
                     <p>Hello broccoli salad!Broccoli salad with a sweet and tangy dressing is my new go to for summer parties. </p>
-                    <p style={{ color: "#da3743", fontWeight: "700", textTransform: "uppercase" }}>Read More</p>
+                    <p style="readmore__button">Read More</p>
                 </div>
                 <div className="blog__card">
                     <img src={Fries} alt="" />
                     <h2> <span style={{ color: "#da3743" }}>Design</span> <span style={{ color: "rgba(85, 100, 128, 1)" }}>01 October 2019</span></h2>
                     <h1>Better Than Takeout Kung Pao Chicken</h1>
                     <p>This super easy version of dan dan noodles is fast, flavorful, and vegetarian to boot.It’s definitely </p>
-                    <p style={{ color: "#da3743", fontWeight: "700", textTransform: "uppercase" }}>Read More</p>
+                    <p style="readmore__button">Read More</p>
                 </div>
             </div>
 
